0f3b6b0ea0863fd0b3ce568b8397435a064fd898
arudo06 <instagramaldz@gmail.com>
2022-12-07 20:42:39 +0700

    mobile

---
+++ lib/data/resep.dart
@@ -0,0 +1,30 @@
+class resep {
+  String name, htm, tutorial, image;
+
+  resep(
+      {required this.name,
+      required this.htm,
+      required this.tutorial,
+      required this.image});
+}
+
+List<resep> dataResep = [
+  resep(
+      name: 'Toyota Yaris',
+      htm: 'Rp 200Jt',
+      tutorial:
+          'Toyota Yaris tersedia dalam pilihan mesin Bensin di Indonesia Hatchback baru dari Toyota hadir dalam 8 varian. Bicara soal spesifikasi mesin Toyota Yaris, ini ditenagai dua pilihan mesin Bensin berkapasitas 1496 cc. Yaris tersedia dengan transmisi CVT and Manual tergantung variannya. Yaris adalah Hatchback 5 seater dengan panjang 4145 mm, lebar 1730 mm, wheelbase 2550 mm',
+      image: 'toyota-yaris.jpg'),
+  resep(
+      name: 'Toyota Yaris',
+      htm: 'Rp 200Jt',
+      tutorial:
+          'Toyota Yaris tersedia dalam pilihan mesin Bensin di Indonesia Hatchback baru dari Toyota hadir dalam 8 varian. Bicara soal spesifikasi mesin Toyota Yaris, ini ditenagai dua pilihan mesin Bensin berkapasitas 1496 cc. Yaris tersedia dengan transmisi CVT and Manual tergantung variannya. Yaris adalah Hatchback 5 seater dengan panjang 4145 mm, lebar 1730 mm, wheelbase 2550 mm',
+      image: 'toyota-yaris.jpg'),
+  resep(
+      name: 'Toyota Yarie',
+      htm: 'Rp 200Jt',
+      tutorial:
+          'Toyota Yaris tersedia dalam pilihan mesin Bensin di Indonesia Hatchback baru dari Toyota hadir dalam 8 varian. Bicara soal spesifikasi mesin Toyota Yaris, ini ditenagai dua pilihan mesin Bensin berkapasitas 1496 cc. Yaris tersedia dengan transmisi CVT and Manual tergantung variannya. Yaris adalah Hatchback 5 seater dengan panjang 4145 mm, lebar 1730 mm, wheelbase 2550 m',
+      image: 'toyota-yaris.jpg')
+];
